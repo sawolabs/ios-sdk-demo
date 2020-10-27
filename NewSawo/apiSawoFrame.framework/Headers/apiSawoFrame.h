5435c885912f287c2b24218958c5b0c856592f16
Soumaya Ranjan <soumaya13x@gmail.com>
2020-10-27 11:05:24 +0530

    initial commit

---
+++ NewSawo/apiSawoFrame.framework/Headers/apiSawoFrame.h
@@ -0,0 +1,19 @@
+//
+//  apiSawoFrame.h
+//  apiSawoFrame
+//
+//  Created by Rhytthm Mahajan on 05/10/20.
+//  Copyright © 2020 SAWO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for apiSawoFrame.
+FOUNDATION_EXPORT double apiSawoFrameVersionNumber;
+
+//! Project version string for apiSawoFrame.
+FOUNDATION_EXPORT const unsigned char apiSawoFrameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <apiSawoFrame/PublicHeader.h>
+
+
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ios-sdk-demo
+
+#### Sawo Passless Demo For iOS Devices
+
+##### Testing the demo
+
+Clone the project, import the project in Xcode and in View ViewController add an API Key
