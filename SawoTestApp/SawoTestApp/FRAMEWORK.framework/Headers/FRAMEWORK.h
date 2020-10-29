062164154b940c108539392c2140ee70e6a46357
rhytthm <50865136+rhytthm@users.noreply.github.com>
2020-10-29 15:13:54 +0530

    Add files via upload

---
+++ SawoTestApp/SawoTestApp/FRAMEWORK.framework/Headers/FRAMEWORK.h
@@ -0,0 +1,18 @@
+//
+//  FRAMEWORK.h
+//  FRAMEWORK
+//
+//  Created by Rhytthm Mahajan on 28/10/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FRAMEWORK.
+FOUNDATION_EXPORT double FRAMEWORKVersionNumber;
+
+//! Project version string for FRAMEWORK.
+FOUNDATION_EXPORT const unsigned char FRAMEWORKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FRAMEWORK/PublicHeader.h>
+
+
